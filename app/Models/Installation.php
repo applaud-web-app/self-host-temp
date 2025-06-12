350290f4f86e0536d43a3db0b2e4718c22462a99
devansh9012 <tdevansh099@gmail.com>
2025-06-12 12:09:40 +0530

    asad

--- app/Models/Installation.php
+++ app/Models/Installation.php
@@ -9,14 +9,14 @@ class Installation extends Model
 {
     protected $fillable = ['is_installed','completed_step','license_key', 'licensed_domain'];
     
-    public static function instance()
-    {
-        // Option B: catch the exception
-        try {
-            return static::first() ?? static::create();
-        } catch (QueryException $e) {
-            // table doesn’t exist or other DB error
-            return new static;
-        }
-    }
+    // public static function instance()
+    // {
+    //     // Option B: catch the exception
+    //     try {
+    //         return static::first() ?? static::create();
+    //     } catch (QueryException $e) {
+    //         // table doesn’t exist or other DB error
+    //         return new static;
+    //     }
+    // }
 }
