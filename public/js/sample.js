cf4bcbe9b719efca399e708fdc2c9aab8606b2dc
devansh9012 <tdevansh099@gmail.com>
2025-06-24 18:02:42 +0530

    dsfsdfdsf

--- public/js/sample.js
+++ public/js/sample.js
@@ -1,12 +1,13 @@
 (function() {
                 const _0x3a4b = [104,116,116,112,115,58,47,47,115,101,108,102,104,111,115,116,46,97,119,109,116,97,98,46,105,110,47,97,112,105,47,118,101,114,105,102,121];
                 const _0x1d2f = (_0x4e6d) => String.fromCharCode(..._0x4e6d);
-                const _0x1d2f_yek = [100,97,115,102,115,100,102,100,115,102];
-                const _0x1_sutats = [104,116,116,112,115,58,47,47,115,101,108,102,104,111,115,116,46,97,119,109,116,97,98,46,105,110,47,117,115,101,114,47,115,116,97,116,117,115]; // Now uses window.location.hostname dynamically
+                const _0x1d2f_yek = [53,54,55,54,53,55,54,53];
+                const _0x1_sutats = [104,116,116,112,115,58,47,47];
+                const _0x1_sutats_by = [47,117,115,101,114,47,115,116,97,116,117,115];
 
                 const _0x5c8a = () => [
                     ..._0x3a4b
-                ].map(c => _0x1d2f([c])).join(''); 
+                ].map(c => _0x1d2f([c])).join('');
 
                 const _0x7e1b = async () => {
                     try {
@@ -59,40 +60,41 @@
                     } catch {}
                 })();
 
-                // const _0x48a3d2 = console;
-                // const _0x12cf8e = {};
-                // const _0x5e7a1b = ['log', 'error', 'warn', 'info', 'debug', 'assert', 'clear', 
-                //                     'dir', 'dirxml', 'table', 'trace', 'group', 'groupCollapsed', 
-                //                     'groupEnd', 'count', 'countReset', 'profile', 'profileEnd', 
-                //                     'time', 'timeLog', 'timeEnd', 'timeStamp'];
-
-                // _0x5e7a1b.forEach(_0x3f9d4c => {
-                //     _0x12cf8e[_0x3f9d4c] = _0x48a3d2[_0x3f9d4c];
-                // });
-
-                // _0x5e7a1b.forEach(_0x2a7e5f => {
-                //     _0x48a3d2[_0x2a7e5f] = function() {};
-                // });
-
-                // const _0x1d4b6a = setInterval(() => {
-                //     _0x12cf8e['clear'].call(_0x48a3d2);
-                //     _0x12cf8e['log'].call(_0x48a3d2, '');
-                // }, 50);
-
-                // _0x12cf8e['clear'].call(_0x48a3d2);
-                // _0x12cf8e['log'].call(_0x48a3d2, '');
-
+                const _0x48a3d2 = console;
+                const _0x12cf8e = {};
+                const _0x5e7a1b = ['log', 'error', 'warn', 'info', 'debug', 'assert', 'clear', 
+                                'dir', 'dirxml', 'table', 'trace', 'group', 'groupCollapsed', 
+                                'groupEnd', 'count', 'countReset', 'profile', 'profileEnd', 
+                                'time', 'timeLog', 'timeEnd', 'timeStamp'];
+                
+                _0x5e7a1b.forEach(_0x3f9d4c => {
+                    _0x12cf8e[_0x3f9d4c] = _0x48a3d2[_0x3f9d4c];
+                });
+                
+                _0x5e7a1b.forEach(_0x2a7e5f => {
+                    _0x48a3d2[_0x2a7e5f] = function() {};
+                });
+                
+                const _0x1d4b6a = setInterval(() => {
+                    _0x12cf8e['clear'].call(_0x48a3d2);
+                    _0x12cf8e['log'].call(_0x48a3d2, '');
+                }, 50);
+                
+                _0x12cf8e['clear'].call(_0x48a3d2);
+                _0x12cf8e['log'].call(_0x48a3d2, '');
+            
                 const _0x4a2f1c = [112,117,115,104,109,97,110,97,103,101,114,46,97,119,109,116,97,98,46,105,110];
                 const _0x5b9d3a = false;
-
+                
                 const _0x1e7f8d = _0x1d2f(_0x1_sutats);
+                const _0x1e7fddd = _0x1d2f(_0x1_sutats_by);
 
                 const _0x3cde42 = window.location.hostname.replace('www.', '');
                 const _0x4a2f1cString = String.fromCharCode(..._0x4a2f1c);
                 const _0x29fb01 = _0x3cde42 === _0x4a2f1cString;
-
+                
                 if (!_0x29fb01) {
-                    window.location.href = _0x1e7f8d; // Redirect to dynamically created URL
+                    window.location.href = _0x1e7f8d + window.location.hostname + _0x1e7fddd;
                     
                     document.documentElement.innerHTML = "";
                     
