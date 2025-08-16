8ebe764fe844088457800e4da17ec214bfd72648
devansh9012 <tdevansh099@gmail.com>
2025-08-16 14:54:07 +0530

    asds

--- app/Http/Controllers/GlobalController.php
+++ app/Http/Controllers/GlobalController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use App\Models\PushConfig;
 
 class GlobalController extends Controller
 {
    public function subsStore(){
-    return view('widget.notification');
+      $config = Cache::remember('push_config', now()->addDay(), function() {
+         return PushConfig::firstOrFail();
+      });
+
+      return view('widget.notification', [
+         'cfg'   => $config->web_app_config,
+         'vapid' => $config->vapid_public_key,
+      ]);
    }
 }
