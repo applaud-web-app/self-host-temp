5a1bf8df612f1440b17cc4a55376d1c90b269875
applaud-web-app <101401063+applaud-web-app@users.noreply.github.com>
2025-08-16 14:26:54 +0530

    dd

--- resources/views/errors/module-unlicensed.blade.php
+++ resources/views/errors/module-unlicensed.blade.php
@@ -3,6 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/favicon_io/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicon_io/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon_io/favicon-16x16.png') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('/images/favicon.ico') }}">
     <title>License Required | {{ config('app.name', 'Our Platform') }}</title>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
