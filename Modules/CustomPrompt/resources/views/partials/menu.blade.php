83e425d36cebd7112c777043544d044d46bc212c
devansh9012 <tdevansh099@gmail.com>
2025-10-14 11:35:49 +0530

    asasd

--- Modules/CustomPrompt/resources/views/partials/menu.blade.php
+++ Modules/CustomPrompt/resources/views/partials/menu.blade.php
@@ -1,6 +1,6 @@
-<li>
+{{-- <li>
     <a class="ai-icon" href="{{ route('customprompt.index') }}" aria-expanded="false">
         <i class="fal fa-browser"></i>
         <span class="nav-text">Customize Prompt</span>
     </a>
-</li>
+</li> --}}
